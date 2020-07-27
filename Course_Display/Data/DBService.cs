33b06ad3c7dba274c1d8520a20a923071baf0756
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2020-07-27 10:45:00 -0500

    added PrereqCheck function to return true/false based on a given list of classes Updated DBService to allow for checking certain tables based on the class code, rather than switch structure

--- Course_Display/Data/DBService.cs
+++ Course_Display/Data/DBService.cs
@@ -13,65 +13,21 @@ namespace Course_Display.Data
             _context = context;
         }
 
-        public async Task<Course> GetClassAsync(string CourseName)
-        {
-            var course = new Course();
-            if (CourseName.StartsWith("CSCI"))
-            {
-                return _context.CSCIList.First(e => e.CourseName == CourseName);
-            }
-            else if (CourseName.StartsWith("CYBR"))
-            {
-                return _context.CYBRList.First(e => e.CourseName == CourseName);
-            }
-            else if (CourseName.StartsWith("CIST"))
-            {
-                return _context.CISTList.First(e => e.CourseName == CourseName);
-            }
-            else if (CourseName.StartsWith("MATH"))
-            {
-                return _context.MATHList.First(e => e.CourseName == CourseName);
-            }
-
-            if (course == null)
-            {
-                course.CourseName = "Missing from Database";
-                course.CreditHours = 0;
-                course.Prereqs = "";
-            }
-
-            return course;
-        }
 
         public Task<Course> GetCourseAsync(string CourseName)
 
         {
             Course course = new Course();
 
-            string strConn = "Data Source = database-1.cinez0pdciee.us-east-2.rds.amazonaws.com,1433; Initial Catalog = CourseDB; User ID = admin; Password = **OrigamiTigerKing**";
+            string strConn = "Data Source = database-1.cinez0pdciee.us-east-2.rds.amazonaws.com,1433; Initial Catalog = CourseDB; User ID = admin; Password = **OrigamiTigerKing**;";
 
             SqlConnection conn = new SqlConnection(strConn);
 
-            string DBToCheck = "";
+            string[] CourseNameArray = CourseName.Split(" ");
 
-            if (CourseName.StartsWith("CSCI"))
-            {
-                DBToCheck = "CSCIList";
-            }
-            else if (CourseName.StartsWith("CYBR"))
-            {
-                DBToCheck = "CYBRList";
-            }
-            else if (CourseName.StartsWith("CIST"))
-            {
-                DBToCheck = "CISTList";
-            }
-            else if (CourseName.StartsWith("MATH"))
-            {
-                DBToCheck = "MATHList";
-            }
+            string DBToCheck = CourseNameArray[0];
 
-            string sql = "SELECT * FROM " + DBToCheck + " WHERE CourseName='" + CourseName + "';";
+            string sql = "SELECT * FROM " + DBToCheck + "List WHERE CourseName='" + CourseName + "';";
 
             SqlCommand command = new SqlCommand(sql, conn);
 
