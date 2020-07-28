666cfd336d38157831d224bf2f1f0e8edada311a
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2020-07-28 15:46:09 -0500

    Implemented Upload course lists to Database page Janky display of tree by single course listed

---
+++ Course_Display/Data/CacheKeyFactory.cs
@@ -0,0 +1,14 @@
+﻿using Course_Display.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace DynamicModelDemo
+{
+    public class CacheKeyFactory : IModelCacheKeyFactory
+    {
+        public object Create(DbContext context)
+            => context is CoursedbContext CdbContext
+                ? (context.GetType(), CdbContext.CourseList)
+                : (object)context.GetType();
+    }
+}
